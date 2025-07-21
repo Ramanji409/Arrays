156de6c1aa2208657b197fb8faf3c3c39b1e3997
Ramanji409 <ramanjaneyuluthullibilli@gmail.com>
2025-07-21 17:41:19 +0530

    Ignore virtual environment

--- Two Pointers.py
+++ Two Pointers.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
             
 def find_pairs(nums,target):
     for i in range(len(nums)):
@@ -9,3 +10,16 @@ def find_pairs(nums,target):
                 print(i,j)
 nums=[0,4,2,5,6]
 find_pairs(nums,7)
+=======
+            
+def find_pairs(nums,target):
+    for i in range(len(nums)):
+        for j in range(i+1,len(nums)):
+            if nums[i] == nums[j]:
+                    continue
+            
+            elif nums[i]+nums[j] == target:
+                print(i,j)
+nums=[0,4,2,5,6]
+find_pairs(nums,7)
+>>>>>>> 03f848e (Ignore virtual environment)
