554bb4263229348788d8703e321375d78b211b4c
Ramanji409 <ramanjaneyuluthullibilli@gmail.com>
2025-07-22 21:48:17 +0530

    Bubble_sort

---
+++ bubble_sort.py
@@ -0,0 +1,9 @@
+def bubble_sort(arr):
+    n=len(arr)
+    for i in range(len(arr)):
+        for j in range(0,n-1-i):
+            if arr[j] > arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+    return arr
+arr=[4,2,8,7,1,0]
+print(bubble_sort(arr))
